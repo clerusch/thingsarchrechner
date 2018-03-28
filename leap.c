8b97ead522195775a7ae7f59c3f587a453d459c4
clerusch <clemensrubenschumann@googlemail.com>
2018-03-28 13:20:02 +0200

    finally

--- leap.c
+++ leap.c
@@ -5,12 +5,14 @@ int main(int argc, char ** argv){
     if (argc !=2 ){
         printf("Falsche Zahl an parametern angegeben. Bitte nur eine Jahreszahl!");
     }
+    else {
     int jahr = atoi(argv[1]);
     if (((jahr % 4 == 0) && (jahr % 100 != 0)) || (jahr % 400 == 0)) {
-        printf("Das Jahr %s ist ein Schaltjahr :-)", jahr);
+        printf("Das Jahr %d ist ein Schaltjahr :-)", jahr);
     }
     else {
-        printf("Das Jahr %s ist leider kein Schaltjahr :-(", jahr);
+        printf("Das Jahr %d ist leider kein Schaltjahr :-(", jahr);
     }
     return EXIT_SUCCESS;
+    }
 }
