210d3ba1fed97ddb4e142699fbb27a8d89c2c0ab
clerusch <clemensrubenschumann@googlemail.com>
2018-03-28 19:28:05 +0200

    finally

--- choose.c
+++ choose.c
@@ -2,27 +2,28 @@
 #include<math.h>
 #include<stdio.h>
 
-int main(int argc, char ** argv) {
+unsigned long fac (unsigned long a){
+    unsigned long facul = 1;
+    for (unsigned long i=1; i <= a; ++i){
+        facul = facul * i;
+    }
+        return facul;
+}
+
+unsigned long choose(unsigned long n, unsigned long k){
+    return fac(n)/(fac(k)*fac(n-k));
+    
+}
+
+int main(int argc, char **argv) {
     if (argc != 3){
         printf("Falsche Zahl an parametern!");
         return EXIT_FAILURE;
     }
-    unsigned long oben = atoi(argv[1]);
-    unsigned long unten = atoi(argv[2]);
-    unsigned long i = 0;
-    unsigned long sum = 0;
-    for(i=1; i <= (oben-1); ++i){
-        sum += i;
-    }
-    unsigned long fac = 1;
-    for(i=1; i<=unten; ++i){
-        fac *= i;
-    }
-    unsigned long choose(unsigned long n, unsigned long k){
-        unsigned long ergebnis = (powl(n,k) - sum)/fac;
-        printf("%ld ist das Ergebnis.", ergebnis);
-        return EXIT_SUCCESS;
-    }
-    choose(oben, unten);
+    unsigned long oben = atol(argv[1]);
+    unsigned long unten = atol(argv[2]);
+    unsigned long ergebnis = choose(oben, unten);
+    printf("Das Ergebnis ist: %lu", ergebnis);
     return EXIT_SUCCESS;
 }
+
--- leap.c
+++ leap.c
@@ -4,8 +4,9 @@
 int main(int argc, char ** argv){
     if (argc !=2 ){
         printf("Falsche Zahl an parametern angegeben. Bitte nur eine Jahreszahl!");
+        return EXIT_FAILURE;
     }
-    else {
+
     int jahr = atoi(argv[1]);
     if (((jahr % 4 == 0) && (jahr % 100 != 0)) || (jahr % 400 == 0)) {
         printf("Das Jahr %d ist ein Schaltjahr :-)", jahr);
@@ -14,5 +15,4 @@ int main(int argc, char ** argv){
         printf("Das Jahr %d ist leider kein Schaltjahr :-(", jahr);
     }
     return EXIT_SUCCESS;
-    }
 }
